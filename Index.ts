15e3b6be05617f00d6a7b2128b92ce34c730be37
ros4x <143718182+ros4x@users.noreply.github.com>
2024-02-05 14:26:33 -0300

    Add files via upload

---
+++ Index.ts
@@ -0,0 +1,16 @@
+import { Animal } from "./Animal";
+import { Mammal } from "./Mammal";
+import { Bird } from "./Bird";
+
+const mammal= new Mammal ("Elephant", 15, 600, "Grayish")
+const bird = new Bird ("Eagle", 10, 7, 2);
+
+console.log(`Name: ${mammal.getName()}, Age: ${mammal.getAge()}`);
+mammal.breastFeed
+
+console.log(`          `);
+mammal.getName("Leopard")
+mammal.setAge(5)
+mammal.setCoat("Yellow")
+console.log(`Name: ${mammal.getName()}, Age: ${mammal.getAge()}`);
+
